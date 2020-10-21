4de2bd8fa13e1fca60fc391e4415da682b34d3c8
Andrea <delorean@featurejam.com>
2020-10-21 15:50:58 +0200

    Export data in numpy array

--- extract_data.py
+++ extract_data.py
@@ -1,3 +1,4 @@
+import pickle
 from collections import defaultdict
 
 from obspy import read, Stream
@@ -6,35 +7,52 @@ import numpy as np
 import pandas as pd
 import glob, os, re
 import matplotlib.pyplot as plt
+from progressbar import ProgressBar
+from scipy import signal
+import numpy as np
 
 DIR = "/mnt/big-data/earthquake/EQ_Exp/"
-explosions = ['Explosion_Early_Final', 'Explosions']
-# for explosion in explosions:
-#     exp_events = glob.glob(os.path.join(DIR, explosion + '/*'))
-#     for event in exp_events:
-#         st = read(event, format='MSEED')
-#         st.filter('bandpass', freqmin=1, freqmax=20)
-#         st.resample(25.0)
-#         stations = defaultdict(lambda: dict())
-#         for tr in st:
-#             stations[tr.stats.station][tr.stats.channel] = tr.data
-#             print(len(tr.data))
-# max 3 channels per signal
-# channels have specific name
-# if vertical channel is missing, replace with 0s
-# if horizontal channel is missing, replace with other hotizontal
-# if only one channel, delete it
-
-earthquakes = ['Earthquakes']
-for earthquake in earthquakes:
-    exp_events = glob.glob(os.path.join(DIR, earthquake + '/*'))
-    for event in exp_events:
-        event_ss = event.replace(earthquake, 'Earthquake_Start_Stop').replace('mseed', 'csv')
-        df = pd.read_csv(event_ss)
+explosions = ['Explosions_Final']  # ,'Explosion_Early_Final'] , 'Explosions']
+ex = {}
+for explosion in explosions:
+    exp_events = glob.glob(os.path.join(DIR, explosion + '/*'))
+    bar = ProgressBar(max_value=len(exp_events))
+    for event in bar(exp_events):
         st = read(event, format='MSEED')
         st.filter('bandpass', freqmin=1, freqmax=20)
         st.resample(25.0)
         stations = defaultdict(lambda: dict())
+        ex[event] = stations
+        for tr in st:
+            stations[tr.stats.station][tr.stats.channel] = tr.data
+
+data_exp = np.empty((0, 3, 2500), float)
+bar = ProgressBar(max_value=len(ex))
+for name, stations in bar(ex.items()):
+    for key in stations.keys():
+        sign = list(stations[key].values())
+        for i in range(0, len(sign), 3):
+            # data_exp.append(np.array([sign[i], sign[i + 1], sign[i + 2]]))
+            new = np.zeros((1, 3, 2500))
+            new[0, 0, :min(sign[i].shape[0], 2500)] = sign[i][:min(sign[i].shape[0], 2500)]
+            new[0, 1, :min(sign[i + 1].shape[0], 2500)] = sign[i + 1][:min(sign[i + 1].shape[0], 2500)]
+            new[0, 2, :min(sign[i + 2].shape[0], 2500)] = sign[i + 2][:min(sign[i + 2].shape[0], 2500)]
+            # new = np.array([np.array([sign[i][:2500], sign[i + 1][:2500], sign[i + 2][:2500]])])
+            data_exp = np.append(data_exp, new, axis=0)
+
+earthquakes = ['Earthquakes']
+eq = {}
+for earthquake in earthquakes:
+    exp_events = glob.glob(os.path.join(DIR, earthquake + '/*'))
+    bar = ProgressBar(max_value=len(exp_events))
+    for event in bar(exp_events):
+        event_ss = event.replace(earthquake, 'Earthquake_Start_Stop').replace('mseed', 'csv')
+        df = pd.read_csv(event_ss)
+        st = read(event, format='MSEED')
+        st.filter('bandpass', freqmin=1, freqmax=20)
+        # st.resample(25.0)
+        stations = defaultdict(lambda: dict())
+        eq[event] = stations
         for tr in st:
             df_station = df[df['Station'] == tr.stats.station]
             if len(df) < 2:
@@ -49,11 +67,22 @@ for earthquake in earthquakes:
                             microsecond=int(str(df_sta['Sec'].values[0]).split('.')[1]))
             tr.trim(p, p + 40, pad=True, fill_value=0)
             stations[tr.stats.station][tr.stats.channel] = tr.data
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            ax.plot(tr.times('matplotlib'), tr.data)
-            ax.xaxis_date()
-            ax.set_xlabel('Time (s)')
-            ax.set_ylabel('Count')
-            plt.xticks(rotation=0)
-            plt.show()
+
+data_eq = np.empty((0, 3, 2500), float)
+bar = ProgressBar(max_value=len(eq))
+for name, stations in bar(eq.items()):
+    for key in stations.keys():
+        sign = list(stations[key].values())
+        for i in range(0, len(sign), 3):
+            new = np.zeros((1, 3, 2500))
+            new[0, 0, :min(sign[i].shape[0], 2500)] = sign[i][:min(sign[i].shape[0], 2500)]
+            new[0, 1, :min(sign[i + 1].shape[0], 2500)] = sign[i + 1][:min(sign[i + 1].shape[0], 2500)]
+            new[0, 2, :min(sign[i + 2].shape[0], 2500)] = sign[i + 2][:min(sign[i + 2].shape[0], 2500)]
+            # data_eq = np.append(data_eq, np.array([sign[i], sign[i + 1], sign[i + 2]]), axis=0)
+            data_eq = np.append(data_eq, new, axis=0)
+
+
+with open('explosions.pkl', 'wb') as f:
+    pickle.dump(data_exp, f, pickle.HIGHEST_PROTOCOL)
+with open('earthquake.pkl', 'wb') as f:
+    pickle.dump(data_eq, f, pickle.HIGHEST_PROTOCOL)
