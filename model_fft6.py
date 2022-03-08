93b92a597391bb99fd986aac78560800bf1329a8
Deniz Ertuncay <dertuncay@gmail.com>
2022-03-08 08:51:46 +0100

    Update model_fft6.py

--- model_fft6.py
+++ model_fft6.py
@@ -12,26 +12,20 @@ def build_nn(input_shape1,input_shape2):
 	pool11 = MaxPooling1D(pool_size=4)(A1)
 	A2 = Conv1D(32, kernel_size=12, activation='relu', kernel_initializer='glorot_normal')(pool11)
 	pool12 = MaxPooling1D(pool_size=2)(A2)
-#	A3 = Conv1D(32, kernel_size=6, activation='relu', kernel_initializer='glorot_normal')(pool12)
-#	pool13 = MaxPooling1D(pool_size=2)(A3)
-	A4 = Conv1D(16, kernel_size=6, activation='relu', kernel_initializer='glorot_normal')(pool12)
-	pool14 = MaxPooling1D(pool_size=2)(A4)
+	A3 = Conv1D(16, kernel_size=6, activation='relu', kernel_initializer='glorot_normal')(pool12)
+	pool13 = MaxPooling1D(pool_size=2)(A3)
 
-	flattened_wf = Flatten()(pool14)  
-	# flattened_wf = Reshape((1, -1))(flattened_wf)
+	flattened_wf = Flatten()(pool13)  
 
 	input2 = Input(shape=input_shape2,name='input2')
 	B1 = Conv1D(8, kernel_size=4, activation='relu', kernel_initializer='glorot_normal')(input2)
 	pool21 = MaxPooling1D(pool_size=4)(B1)
 	B2 = Conv1D(4, kernel_size=2, activation='relu', kernel_initializer='glorot_normal')(pool21)
 	pool22 = MaxPooling1D(pool_size=2)(B2)
-#	B3 = Conv1D(32, kernel_size=6, activation='relu', kernel_initializer='glorot_normal')(pool22)
-#	pool23 = MaxPooling1D(pool_size=2)(B3)
-	B4 = Conv1D(2, kernel_size=1, activation='relu', kernel_initializer='glorot_normal')(pool22)
-	pool24 = MaxPooling1D(pool_size=2)(B4)
+	B3 = Conv1D(2, kernel_size=1, activation='relu', kernel_initializer='glorot_normal')(pool23)
+	pool23 = MaxPooling1D(pool_size=2)(B3)
 
 	flattened_fft = Flatten()(pool24) 
-	# flattened_spec = Reshape((1, -1))(flattened_spec)
 
 	concatted = concatenate([flattened_wf, flattened_fft], axis = 1)
 
@@ -48,7 +42,6 @@ def build_nn(input_shape1,input_shape2):
 			keras.metrics.binary_accuracy
 		])
 	return model
-# plot_model(model,to_file='demo.png',show_shapes=True)
 
 def get_early_stop():
 	return EarlyStopping(monitor='val_loss', patience=2, verbose=0, mode='auto', restore_best_weights=True)	
